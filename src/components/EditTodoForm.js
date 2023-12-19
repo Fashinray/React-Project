8c27ae01f9309f3f7ebae96e83ab7e1d30761a65
Fashinray <111636176+Fashinray@users.noreply.github.com>
2023-12-19 03:52:34 +0100

    Update EditTodoForm.js

--- src/components/EditTodoForm.js
+++ src/components/EditTodoForm.js
@@ -6,7 +6,7 @@ export const EditTodoForm = ({editTodo, task}) => {
     const handleSubmit = (e) => {
       // prevent default action
         e.preventDefault();
-        // edit todo
+    //
         editTodo(value, task.id);
       };
   return (
